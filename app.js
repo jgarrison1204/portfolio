82877d6a37efd4e83f3249f5a29e4097e4ccd448
jgarrison1204 <jgarrison1204@gmail.com>
2017-01-12 15:20:36 -0800

    Animations for close functional

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ $("#test").click(function(){
 	$(this).toggle();
 });
 $("#crm-project").click(function(){
-	console.log(this);
+	document.getElementById("overlay").style.width = "100%";
+	document.getElementById("overlay").style.height = "100%";
+	$("#close-btn-wrapper").removeClass("spin");
+})
+$("#close-btn-wrapper").click(function(){
+	$(this).addClass("spin");
+	$("#overlay").delay(1000).animate({width: "0px"});
 })
