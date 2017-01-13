497115e32bb0c39bc45e6203911e0f5e20f5e16d
jgarrison1204 <jgarrison1204@gmail.com>
2017-01-13 11:04:50 -0800

    Added jQuery fadeIn and adjusted close of overlay to enable fadeIn()

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ $("#test").click(function(){
 	$(this).toggle();
 });
 $("#crm-project").click(function(){
-	document.getElementById("overlay").style.width = "100%";
-	document.getElementById("overlay").style.height = "100%";
+	// document.getElementById("overlay").style.width = "100%";
+	// document.getElementById("overlay").style.height = "100%";
+	$(".overlay").fadeIn(2000);
 	$("#close-btn-wrapper").removeClass("spin");
 })
 $("#close-btn-wrapper").click(function(){
 	$(this).addClass("spin");
-	$("#overlay").delay(1000).animate({width: "0px"});
+	$("#overlay").delay(1000).animate({
+		width: 'hide'
+	});
 })
 $('.carousel').carousel({
   interval: 2000
+})
+$("#logo").click(function(){
+	$(".test").fadeToggle("ease");
 })
