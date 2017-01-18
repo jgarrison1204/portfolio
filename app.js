3485710a22fa5e5ce3388288c01cf6128f47f9cd
jgarrison1204 <jgarrison1204@gmail.com>
2017-01-17 16:22:27 -0800

    Basic header and nav started

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ $("#test").click(function(){
 	$(this).toggle();
 });
 $("#crm-project").click(function(){
-	// document.getElementById("overlay").style.width = "100%";
-	// document.getElementById("overlay").style.height = "100%";
 	$(".overlay").fadeIn(2000);
 	$("#close-btn-wrapper").removeClass("spin");
 })
@@ -15,7 +13,4 @@ $("#close-btn-wrapper").click(function(){
 })
 $('.carousel').carousel({
   interval: 2000
-})
-$("#logo").click(function(){
-	$(".test").fadeToggle("ease");
 })
